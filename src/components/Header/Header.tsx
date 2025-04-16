c444084639e79c2380f43797e472352027412f8e
123 <a123@123s-MacBook-Air.local>
2025-04-16 12:35:59 +0500

    fix error

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,9 @@
 
 import React, { useEffect } from "react";
 import Image from "next/image";
-import { orbitron } from "@/app/page";
 import clsx from "clsx";
 import Link from "next/link";
+import { orbitron } from "@/app/page";
 
 export const Header: React.FC = () => {
   useEffect(() => {
